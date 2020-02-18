58064fd65d57525783b7a57d307475bb1630372d
Adam Hwang <github@adamhwang.com>
2020-02-18 15:57:40 -0600

    Added csrf+session auth method

--- index.js
+++ index.js
@@ -6,14 +6,39 @@ async function uploadOperaExtension (options) {
   const browser = await puppeteer.launch({ headless: !debugModeEnabled })
   const page = await browser.newPage()
 
+  const url = `https://addons.opera.com/developer/package/${options.extensionId}/`;
+  const cookie = [];
+
+  if (process.env.OPERA_CSRF_TOKEN) {
+    cookie.push({
+      url,
+      name: "csrftoken",
+      value: process.env.OPERA_CSRF_TOKEN
+    })
+  }
+
+  if (process.env.OPERA_SESSION_ID) {
+    cookie.push({
+      url,
+      name: "sessionid",
+      value: process.env.OPERA_SESSION_ID
+    })
+  }
+
+  if (cookie.length) {
+    await page.setCookie(...cookie)
+  }
+
   try {
     // Directly go to the extension page - the browser will be redirected to the auth page and back once the login succeeded
-    await page.goto(`https://addons.opera.com/developer/package/${options.extensionId}/`)
+    await page.goto(url)
 
-    // Perform login
-    await page.type('input[name=email]', options.email)
-    await page.type('input[name=password]', options.password)
-    await page.click('button[type=submit]')
+    if (!cookie.length) {
+      // Perform login
+      await page.type('input[name=email]', options.email)
+      await page.type('input[name=password]', options.password)
+      await page.click('button[type=submit]')
+    }
 
     // Wait for, and then click, "Versions"
     await page.waitForSelector('ul.nav .uib-tab:nth-child(2) a')
